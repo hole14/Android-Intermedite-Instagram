730fdf3ad2b0b825a70e0cd97c49e544f19a2d79
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-02-11 14:17:38 +0700

    Menambahkan fragment pada activity_main.xml

--- app/src/main/java/com/example/myinstagram/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/myinstagram/fragment/HomeFragment.kt
@@ -5,15 +5,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.myinstagram.R
+import com.example.myinstagram.adapter.AdapterStatus
+import com.example.myinstagram.adapter.tampilan
+import com.example.myinstagram.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
-
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var status: RecyclerView
+    private val list = ArrayList<tampilan>()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        status = binding.recyclerStatus
+        status.setHasFixedSize(true)
+
+        list.addAll(tampilin())
+        daftarnya()
+    }
+    private fun daftarnya(){
+        status.layoutManager = LinearLayoutManager(requireActivity(), LinearLayoutManager.HORIZONTAL, false)
+        status.adapter = AdapterStatus(list)
+    }
+
+    private fun tampilin(): ArrayList<tampilan> {
+        val dataJudul = resources.getStringArray(R.array.data_user)
+        val dataPhoto = resources.obtainTypedArray(R.array.data_status)
+        val listTampil = ArrayList<tampilan>()
+        for (i in dataJudul.indices) {
+            val tampil = tampilan(dataJudul[i], dataPhoto.getResourceId(i, -1))
+            listTampil.add(tampil)
+        }
+        return listTampil
     }
 }
